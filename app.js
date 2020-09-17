1aad3c1aa1eac667c12a5ecc7a0d79b9ff3ccd04
James Mernin <jamespmernin@gmail.com>
2020-09-17 19:10:52 -0400

    Completed base requirements

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
 const DOMAIN = 'http://www.omdbapi.com/';
-const API_KEY = /* Your API key goes here */
+const API_KEY = 'd715f08c';
 const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&`;
 
+function renderList(movies) {
+  let movieList = document.querySelector('.movie-list');
+  for (let i = 0; i < movies.length; i++) {
+    let movieResult = document.createElement('div');
+    let moviePoster = document.createElement('img');
+    moviePoster.src = movies[i].Poster;
+    let movieTitle = document.createElement('p');
+    movieTitle.textContent = movies[i].Title;
+    let movieYear = document.createElement('p');
+    movieYear.textContent = movies[i].Year;
+    movieResult.append(moviePoster);
+    movieResult.append(movieTitle);
+    movieResult.append(movieYear);
+    movieList.append(movieResult);
+  }
+}
 
+async function parseResult(searchResult) {
+  try {
+    let movies = await axios.get(`${BASE_URL}s=${searchResult}`);
+    console.log(movies.data.Search);
+    renderList(movies.data.Search);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  } finally {
+    console.log('Finally got it.');
+  }
+}
+
+function movieSearch() {
+  submission = document.querySelector('.search-section').addEventListener('submit', (e) => {
+    e.preventDefault();
+    parseResult(document.querySelector('#blank').value);
+  });
+}
+
+movieSearch();
